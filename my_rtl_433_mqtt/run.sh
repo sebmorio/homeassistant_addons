2bb79ee260d09a8ff474115fcd9af971c313a6b7
root <root@testdeb1.home>
2025-02-19 18:46:14 +0100

    Rajout timezone + id

--- my_rtl_433_mqtt/run.sh
+++ my_rtl_433_mqtt/run.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-MQTT_HOST=$(bashio::config 'mqtt_host')
-MQTT_PORT=$(bashio::config 'mqtt_port')
-MQTT_USER=$(bashio::config 'mqtt_user')
-MQTT_PASS=$(bashio::config 'mqtt_password')
+MQTT_HOST=$(jq -r '.mqtt_host' /data/options.json)
+MQTT_PORT=$(jq -r '.mqtt_port' /data/options.json)
+MQTT_USER=$(jq -r '.mqtt_user' /data/options.json)
+MQTT_PASS=$(jq -r '.mqtt_pass' /data/options.json)
+
+#MQTT_HOST=$(bashio::config 'mqtt_host')
+#MQTT_PORT=$(bashio::config 'mqtt_port')
+#MQTT_USER=$(bashio::config 'mqtt_user')
+#MQTT_PASS=$(bashio::config 'mqtt_password')
 
 MQTT_CLIENT_ID="rtl_433_$(hostname)"  # Utilise le nom du conteneur ou un préfixe
 MQTT_URL="mqtt://${MQTT_USER}:${MQTT_PASS}@${MQTT_HOST}:${MQTT_PORT},retain=0,client_id=${MQTT_CLIENT_ID}"
