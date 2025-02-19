87e1e87bded8b7f62697ce3b49cfb2c35d3d00f4
root <root@testdeb1.home>
2025-02-19 18:30:48 +0100

    Rajout timezone + id

--- my_rtl_433_mqtt/run.sh
+++ my_rtl_433_mqtt/run.sh
@@ -5,6 +5,8 @@ MQTT_PORT=$(bashio::config 'mqtt_port')
 MQTT_USER=$(bashio::config 'mqtt_user')
 MQTT_PASS=$(bashio::config 'mqtt_password')
 
-echo "Démarrage de rtl_433 avec MQTT..."
-rtl_433 -R -215 -F "mqtt://${MQTT_USER}:${MQTT_PASS}@${MQTT_HOST}:${MQTT_PORT},retain=0"
+MQTT_CLIENT_ID="rtl_433_$(hostname)"  # Utilise le nom du conteneur ou un préfixe
+MQTT_URL="mqtt://${MQTT_USER}:${MQTT_PASS}@${MQTT_HOST}:${MQTT_PORT},retain=0,client_id=${MQTT_CLIENT_ID}"
 
+echo "Démarrage de rtl_433 avec MQTT..."
+rtl_433 -R -215 -F "${MQTT_URL}"
