c1a98a443c661fd5ac2b261a9bd6e30c7e2bf258
root <root@testdeb1.home>
2025-02-20 12:20:55 +0100

    Correction

--- my_rtl_433_mqtt/run.sh
+++ my_rtl_433_mqtt/run.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-MQTT_HOST=$(jq -r '.mqtt_host' /data/options.json)
-MQTT_PORT=$(jq -r '.mqtt_port' /data/options.json)
-MQTT_USER=$(jq -r '.mqtt_user' /data/options.json)
-MQTT_PASS=$(jq -r '.mqtt_pass' /data/options.json)
+MQTT_HOST=$(bashio::config 'mqtt_host')
+MQTT_PORT=$(bashio::config 'mqtt_port')
+MQTT_USER=$(bashio::config 'mqtt_user')
+MQTT_PASS=$(bashio::config 'mqtt_password')
+#MQTT_HOST=homeassistant
+#MQTT_PORT=1883
+#MQTT_USER=smorio
+#MQTT_PASS=Chatou78
+MQTT_DEVICE="rtl_433/devices[/type][/model][/subtype][/channel][/id]"
 
-#MQTT_HOST=$(bashio::config 'mqtt_host')
-#MQTT_PORT=$(bashio::config 'mqtt_port')
-#MQTT_USER=$(bashio::config 'mqtt_user')
-#MQTT_PASS=$(bashio::config 'mqtt_password')
+MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=0,devices=${MQTT_DEVICE}"
 
-MQTT_CLIENT_ID="rtl_433_$(hostname)"  # Utilise le nom du conteneur ou un préfixe
-MQTT_URL="mqtt://${MQTT_USER}:${MQTT_PASS}@${MQTT_HOST}:${MQTT_PORT},retain=0,client_id=${MQTT_CLIENT_ID}"
-
+echo "<<< ${MQTT_URL} >>>"
 echo "Démarrage de rtl_433 avec MQTT..."
-rtl_433 -R -215 -F "${MQTT_URL}"
+rtl_433 -R -215 -F "${MQTT_URL}" 
