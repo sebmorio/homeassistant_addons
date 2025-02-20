0942620851541a8a5be60991133dd742481b722c
root <root@testdeb1.home>
2025-02-20 12:21:37 +0100

    Correction

--- my_rtl_433_mqtt/run.sh
+++ my_rtl_433_mqtt/run.sh
@@ -4,10 +4,7 @@ MQTT_HOST=$(bashio::config 'mqtt_host')
 MQTT_PORT=$(bashio::config 'mqtt_port')
 MQTT_USER=$(bashio::config 'mqtt_user')
 MQTT_PASS=$(bashio::config 'mqtt_password')
-#MQTT_HOST=homeassistant
-#MQTT_PORT=1883
-#MQTT_USER=smorio
-#MQTT_PASS=Chatou78
+
 MQTT_DEVICE="rtl_433/devices[/type][/model][/subtype][/channel][/id]"
 
 MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=0,devices=${MQTT_DEVICE}"
