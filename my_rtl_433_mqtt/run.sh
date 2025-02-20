e30d24dd24aa18896eb78760a46fb5bfc272207a
root <root@testdeb1.home>
2025-02-20 18:15:57 +0100

    .

--- my_rtl_433_mqtt/run.sh
+++ my_rtl_433_mqtt/run.sh
@@ -8,8 +8,6 @@ MQTT_PASS=$(jq -r '.mqtt_password' /data/options.json)
 MQTT_DEVICE="rtl_433/devices[/type][/model][/subtype][/channel][/id]"
 
 MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=0,devices=${MQTT_DEVICE}"
-env
-pwd
 echo "<<< ${MQTT_URL} >>>"
 echo "Démarrage de rtl_433 avec MQTT..."
 rtl_433 -R -215 -F "${MQTT_URL}" 
