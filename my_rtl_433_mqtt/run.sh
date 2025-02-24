b87819821dd94b397bfd1ee58f551844b5511d30
root <root@testdeb1.home>
2025-02-24 18:10:15 +0100

    Modif test retain=1

--- my_rtl_433_mqtt/run.sh
+++ my_rtl_433_mqtt/run.sh
@@ -7,7 +7,7 @@ MQTT_PASS=$(jq -r '.mqtt_password' /data/options.json)
 
 MQTT_DEVICE="rtl_433/devices[/type][/model][/subtype][/channel][/id]"
 
-MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=0,devices=${MQTT_DEVICE}"
+MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=1,devices=${MQTT_DEVICE}"
 
 echo "lsusb"
 lsusb
