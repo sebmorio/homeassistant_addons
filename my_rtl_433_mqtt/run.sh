415b2a8f78a65989f5f292b9bd70fd470ae79929
Sébastien MORIO <156841158+sebmorio@users.noreply.github.com>
2026-09-22 23:14:34 +0200

    Update run.sh

--- my_rtl_433_mqtt/run.sh
+++ my_rtl_433_mqtt/run.sh
@@ -12,10 +12,10 @@ MQTT_PASS=$(jq -r '.mqtt_password' /data/options.json)
 #MQTT_DEVICE="rtl_433/devices[/model][/channel][/id]"
 
 #MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=1,devices=${MQTT_DEVICE}"
-##MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=1"
+MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=1"
 
-MQTT_DEVICE="rtl_433/devices[/model][/channel]"
-MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=1,devices=${MQTT_DEVICE},events=rtl_433/mystation/events"
+##MQTT_DEVICE="rtl_433/devices[/model][/channel]"
+##MQTT_URL="mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=1,devices=${MQTT_DEVICE},events=rtl_433/mystation/events"
 
 echo "lsusb"
 lsusb
